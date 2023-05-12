81bcc262f7cc208a46acf0ad451d043a59916168
Maria Sharkova <moriko.tys@gmail.com>
2023-05-12 16:18:42 +0300

    1st review fix

--- backend/recipes/management/commands/load_ingredients.py
+++ backend/recipes/management/commands/load_ingredients.py
@@ -21,7 +21,7 @@ class Command(BaseCommand):
     help = "Load ingredients to DB"
 
     def handle(self, *args, **options):
-        path = os.path.join(settings.BASE_DIR, '../data/ingredients.csv')
+        path = os.path.join(settings.BASE_DIR, '../../data/ingredients.csv')
         with open(path, 'r', encoding='utf-8') as file:
             row_count = sum(1 for row in file)
         with open(path, 'r', encoding='utf-8') as file:
